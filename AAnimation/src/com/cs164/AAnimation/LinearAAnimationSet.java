cd343c2c06dc979d02f8df0ee896e6cb919b2687
Japheth Wong <japhethjwong@gmail.com>
2014-12-08 15:44:31 -0800

    Added class for LinearAAnimationSet (all stubs)

---
+++ AAnimation/src/com/cs164/AAnimation/LinearAAnimationSet.java
@@ -0,0 +1,24 @@
+package com.cs164.AAnimation;
+
+import android.animation.Animator;
+
+import java.util.List;
+
+/**
+ * Created by Japheth on 12/8/2014.
+ */
+public class LinearAAnimationSet implements AAnimationSet {
+    public LinearAAnimationSet(List<Animator> animators, List<AAnimationState> states) {
+        // TODO: Implement me!
+    }
+
+    @Override
+    public void run() {
+        // TODO: Implement me!
+    }
+
+    @Override
+    public void cancel() {
+        // TODO: Implement me!
+    }
+}
