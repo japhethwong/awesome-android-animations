d587b2f49b72bfb780fc4ed6053070c2581d1971
Japheth Wong <japhethjwong@gmail.com>
2014-12-08 14:58:05 -0800

    Adding interface for AAnimationSet

--- AAnimation/src/com/cs164/AAnimation/FadeAAnimation.java
+++ AAnimation/src/com/cs164/AAnimation/FadeAAnimation.java
@@ -28,7 +28,7 @@ public class FadeAAnimation extends BasicAAnimationFactory {
     @Override
     public AAnimationSet apply(ArrayList<View> objects) {
         ArrayList<Animator> animators = new ArrayList<Animator>();
-        ArrayList<AAnimationState> states = new ArrayList<AAnimationState>()
+        ArrayList<AAnimationState> states = new ArrayList<AAnimationState>();
         for (final View view: objects) {
             AAnimationState state = new AAnimationState(view);
             states.add(state);
@@ -58,6 +58,6 @@ public class FadeAAnimation extends BasicAAnimationFactory {
             });
             animators.add(fade);
         }
-        return new AAnimationSet(animators, states);
+        return new ParallelAAnimationSet(animators, states);
     }
 }
