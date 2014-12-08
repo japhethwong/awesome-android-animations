1a838e0deb45d9e81dd3dc3bbd663c1247ad5209
A <amytang0@gmail.com>
2014-12-08 15:34:41 -0800

    Moved duration and wait fields to BasicAAnimation

--- AAnimation/src/com/cs164/AAnimation/RotateAAnimationFactory.java
+++ AAnimation/src/com/cs164/AAnimation/RotateAAnimationFactory.java
@@ -1,5 +1,6 @@
 package com.cs164.AAnimation;
 
+import android.animation.Animator;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -8,4 +9,27 @@ import java.util.ArrayList;
  * Created by amytang on 12/8/14.
  */
 public class RotateAAnimationFactory extends BasicAAnimationFactory {
+    private int rotation;
+
+    public  RotateAAnimationFactory(int rotation, int duration, int wait) {
+        this.rotation = rotation;
+        this.duration = duration;
+        this.wait = wait;
+    }
+
+    public RotateAAnimationFactory(int rotation) {
+        this(rotation, 200, 0);
+    }
+    @Override
+    public AAnimationSet apply(ArrayList<View> objects) {
+        ArrayList<Animator> animators = new ArrayList<Animator>();
+        ArrayList<AAnimationState> states = new ArrayList<AAnimationState>();
+        for (final View view: objects) {
+            AAnimationState state = new AAnimationState(view);
+            states.add(state);
+
+
+        }
+        return new ParallelAAnimationSet(animators, states);
+    }
 }
