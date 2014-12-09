1b14923eb1a991a804364237fb9b7dc375aff1b9
A <amytang0@gmail.com>
2014-12-09 11:05:24 -0800

    set up demo app

--- AAnimationDemo/src/com/example/AAnimationDemo/DemoActivity.java
+++ AAnimationDemo/src/com/example/AAnimationDemo/DemoActivity.java
@@ -2,6 +2,7 @@ package com.example.AAnimationDemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class DemoActivity extends Activity {
@@ -12,6 +13,14 @@ public class DemoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /**
+     * Response handler to react to taps on our button which toggles animations.
+     * @param v is the view which registered the tap
+     */
+    public void onClick(View v) {
+        Log.d("HEY", "got clicked");
     }
 
 }
